c80dca50529535bfcfb33c9af5cbb609c593b1bf
Fran <Franmenchaca2004@gmail.com>
2025-10-31 12:48:12 -0300

    Avance ejercicio 6

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -2,11 +2,90 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/MaxHeap.cpp"
 
 using namespace std;
 
+struct Pozo
+{
+    int inicio;
+    int final;
+};
+
+struct Mejora
+{
+    int posicion;
+    int valor;
+};
+
 int main()
 {
-    // TODO
+    int N;
+    cin >> N;
+    int M;
+    cin >> M;
+    int F;
+    cin >> F;
+    Pozo *pozos = new Pozo[N];
+    Mejora *mejoras = new Mejora[M];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> pozos[i].inicio >> pozos[i].final;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        cin >> mejoras[i].posicion >> mejoras[i].valor;
+    }
+
+    int pos = 1;
+    int potencia = 1;
+    int mejorasUsadas = 0;
+    int indiceMejora = 0;
+    int indicePozo = 0;
+    MaxHeap heap(M);
+
+    while (pos < F)
+    {
+        // Si el próximo pozo empieza en pos + 1
+        //Me fijo si sigue habiendo pozos (índicePozo < N) 
+        if (indicePozo < N && pozos[indicePozo].inicio == pos + 1)
+        {
+            int largoPozo = pozos[indicePozo].final - pozos[indicePozo].inicio + 1;
+
+            // Agregar al heap todas las mejoras que se encuentren antes o en la entrada del pozo
+            while (indiceMejora < M && mejoras[indiceMejora].posicion <= pos)
+            {
+                heap.insertar(mejoras[indiceMejora].valor);
+                indiceMejora++;
+            }
+
+            // Tomar mejoras hasta poder cruzar el pozo
+            while (potencia < largoPozo + 1)
+            {
+                if (heap.estaVacio())
+                {
+                    cout << "Imposible" << endl;
+                    return 0;
+                }
+                potencia += heap.tope();
+                heap.eliminar();
+                mejorasUsadas++;
+            }
+
+            // Cruzar pozo
+            pos = pozos[indicePozo].final + 1;
+            indicePozo++;
+        }
+        // No hay pozo, avanzar 1 a 1
+        else
+        {
+            pos++;
+        }
+    }
+
+    cout << mejorasUsadas << endl;
+
+    delete[] pozos;
+    delete[] mejoras;
     return 0;
 }
---
+++ tads/MaxHeap.cpp
@@ -0,0 +1,126 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+
+class MaxHeap
+{
+private:
+    int *vec;
+    int capacidad;
+    int primeroLibre;
+
+    bool estaLleno()
+    {
+        return this->primeroLibre > this->capacidad;
+    }
+    
+
+    int padre(int pos)
+    {
+        return pos / 2;
+    }
+
+    int funcionComparadora(int a, int b)
+    {
+        return b - a;
+    }
+
+    void intercambiar(int posPadre, int pos)
+    {
+        int aux = vec[posPadre];
+        vec[posPadre] = vec[pos];
+        vec[pos] = aux;
+    }
+
+    void flotar(int pos)
+    {
+        if (pos > 1)
+        {
+            int posPadre = padre(pos);
+            if (funcionComparadora(vec[posPadre], vec[pos]) > 0) // ahora compara al revés
+            {
+                intercambiar(posPadre, pos);
+                flotar(posPadre);
+            }
+        }
+    }
+
+    int izq(int pos)
+    {
+        return pos * 2;
+    }
+
+    int der(int pos)
+    {
+        return pos * 2 + 1;
+    }
+
+    void hundir(int pos)
+    {
+        int posHijoIzq = izq(pos);
+        int posHijoDer = der(pos);
+
+        if (posHijoIzq < this->primeroLibre && posHijoDer < this->primeroLibre)
+        {
+            // elegimos el hijo MAYOR, no el menor
+            int posHijoMayor = funcionComparadora(vec[posHijoIzq], vec[posHijoDer]) > 0 ? posHijoIzq : posHijoDer;
+            if (funcionComparadora(vec[pos], vec[posHijoMayor]) > 0)
+            {
+                intercambiar(pos, posHijoMayor);
+                hundir(posHijoMayor);
+            }
+        }
+        else if (posHijoIzq < this->primeroLibre)
+        {
+            if (funcionComparadora(vec[pos], vec[posHijoIzq]) > 0)
+            {
+                intercambiar(pos, posHijoIzq);
+                hundir(posHijoIzq);
+            }
+        }
+    }
+
+public:
+    MaxHeap(int capacidad)
+    {
+        this->vec = new int[capacidad + 1];
+        this->capacidad = capacidad;
+        this->primeroLibre = 1;
+    }
+
+    ~MaxHeap()
+    {
+        delete[] this->vec;
+    }
+
+    void insertar(int dato)
+    {
+        assert(!this->estaLleno());
+        if (!this->estaLleno())
+        {
+            this->vec[this->primeroLibre] = dato;
+            flotar(this->primeroLibre);
+            this->primeroLibre++;
+        }
+    }
+
+    void eliminar()
+    {
+        assert(!estaVacio());
+        this->vec[1] = this->vec[primeroLibre - 1];
+        this->primeroLibre--;
+        hundir(1);
+    }
+
+    int tope()
+    {
+        assert(this->primeroLibre > 1);
+        return this->vec[1];
+    }
+
+    bool estaVacio()
+    {
+        return this->primeroLibre == 1;
+    }
+};
