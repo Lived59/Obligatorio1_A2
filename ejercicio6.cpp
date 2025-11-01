4ee91d785ea1e46b9c82832cb741ce63f46aca3a
jojosapelli <130329653+jojosapelli@users.noreply.github.com>
2025-11-01 17:24:20 -0300

    ejercicio 6 eficiente

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 struct Pozo
 {
-    int inicio;
-    int final;
+    long long inicio;
+    long long final;
 };
 
 struct Mejora
@@ -32,7 +32,7 @@ int main()
     {
         cin >> pozos[i].inicio >> pozos[i].final;
     }
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i <= M; i++)
     {
         cin >> mejoras[i].posicion >> mejoras[i].valor;
     }
@@ -41,11 +41,13 @@ int main()
     int potencia = 1;
     int mejorasUsadas = 0;
     int indiceMejora = 0;
-    int indicePozo = 0;
     MaxHeap heap(M);
 
-    while (pos < F)
+    for (int indicePozo = 0; indicePozo < N; indicePozo++)
     {
+        pos = pozos[indicePozo].inicio;
+        int largoPozo = pozos[indicePozo].final - pozos[indicePozo].inicio + 1;
+
         // Agregar al heap todas las mejoras que se encuentren antes o en la entrada del pozo
         while (indiceMejora < M && mejoras[indiceMejora].posicion <= pos)
         {
@@ -54,38 +56,23 @@ int main()
         }
 
         // Si el próximo pozo empieza en pos + 1
-        //Me fijo si sigue habiendo pozos (índicePozo < N) 
-        if (indicePozo < N && pozos[indicePozo].inicio == pos + 1)
+        // Tomar mejoras hasta poder cruzar el pozo
+        while (potencia < largoPozo + 1)
         {
-            int largoPozo = pozos[indicePozo].final - pozos[indicePozo].inicio + 1;
-
-            // Tomar mejoras hasta poder cruzar el pozo
-            while (potencia < largoPozo + 1)
+            if (heap.estaVacio())
             {
-                if (heap.estaVacio())
-                {
-                    cout << "Imposible" << endl;
-                    delete[] pozos;
-                    delete[] mejoras;
-                    return 0;
-                }
-                potencia += heap.tope();
-                heap.eliminar();
-                mejorasUsadas++;
+                cout << "Imposible" << endl;
+                delete[] pozos;
+                delete[] mejoras;
+                return 0;
             }
-
-            // Cruzar pozo
-            pos = pozos[indicePozo].final + 1;
-            indicePozo++;
-        }
-        // No hay pozo, avanzar 1 a 1
-        else
-        {
-            pos++;
+            potencia += heap.tope();
+            heap.eliminar();
+            mejorasUsadas++;
         }
     }
 
-    cout << mejorasUsadas << endl;
+    cout <<  mejorasUsadas << endl;
 
     delete[] pozos;
     delete[] mejoras;
