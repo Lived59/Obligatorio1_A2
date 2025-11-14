2c9eb64505be844f052545f833c1195dd9925ee9
Fran <Franmenchaca2004@gmail.com>
2025-11-14 17:31:26 -0300

    tabla de hash abierto y main de ejercicio 7

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,34 +2,39 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/tabla_de_hash_abierto"
 
 using namespace std;
 
-int * mergeSort(int * valores, int inicio, int fin){
-    if(inicio == fin){
-        int * ret = new int[1];
-        ret[0] = valores[inicio];
-        return ret;
-    }
-    int medio = (inicio + fin)/2;
-
-    int *ord1 = mergeSort(valores, inicio, medio);
-    int *ord2 = mergeSort(valores, medio + 1, fin);
-
-    //return intercalar(ord1, ord2);
+int mergeSortCount(int* arr, int* temp, int inicio, int fin){
+    return 0;
 }
 int main()
 {
     int N;
     cin >> N;
+
     string *profesor = new string[N];
     string *ayudante = new string[N];
-    for (int i = 0; i < N; i++) cin >> profesor[i];
-    for (int i = 0; i < N; i++) cin >> ayudante[i];
 
+    for (int i = 0; i < N; i++)cin >> profesor[i];
+    for (int i = 0; i < N; i++)cin >> ayudante[i];
 
-    //Tabla de Hash
-    //Merge-sort contando inversiones
-    
+    HashAbierto tabla(2 * N + 1);
+    for (int i = 0; i < N; i++)tabla.insertar(profesor[i], i);
+
+    int *posiciones = new int[N];
+    for (int i = 0; i < N; i++)
+        posiciones[i] = tabla.buscar(ayudante[i]);
+
+    int *temp = new int[N];
+    int inversiones = mergeSortCount(posiciones, temp, 0, N - 1);
+    cout << inversiones << endl;
+
+    delete[] profesor;
+    delete[] ayudante;
+    delete[] posiciones;
+    delete[] temp;
+
     return 0;
 }
