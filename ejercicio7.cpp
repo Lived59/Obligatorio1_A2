a4af6cbb52b2308f00daf243c63ba3418fddb54d
Fran <Franmenchaca2004@gmail.com>
2025-11-13 23:22:19 -0300

    ejercicio 7 comienzo

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -5,8 +5,31 @@
 
 using namespace std;
 
+int * mergeSort(int * valores, int inicio, int fin){
+    if(inicio == fin){
+        int * ret = new int[1];
+        ret[0] = valores[inicio];
+        return ret;
+    }
+    int medio = (inicio + fin)/2;
+
+    int *ord1 = mergeSort(valores, inicio, medio);
+    int *ord2 = mergeSort(valores, medio + 1, fin);
+
+    //return intercalar(ord1, ord2);
+}
 int main()
 {
-    // TODO
+    int N;
+    cin >> N;
+    string *profesor = new string[N];
+    string *ayudante = new string[N];
+    for (int i = 0; i < N; i++) cin >> profesor[i];
+    for (int i = 0; i < N; i++) cin >> ayudante[i];
+
+
+    //Tabla de Hash
+    //Merge-sort contando inversiones
+    
     return 0;
 }
--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,8 +5,48 @@
 
 using namespace std;
 
+int ***pd; // Inicializado en -1
+int max(int a, int b);
+
+int puntajeMaximo(int inicio, int fin, int acPrevios, int * fragmentos)
+{
+    // Casos base
+    if (inicio > fin)
+        return 0;
+    if (pd[inicio][fin][acPrevios] != -1)
+    {
+        return pd[inicio][fin][acPrevios];
+    }
+    if(inicio == fin){
+        return pd[inicio][fin][acPrevios] = (acPrevios + 1) * (acPrevios + 1);
+    }
+
+    int mejorPuntaje = 0;
+    //Opcion de borrarlo
+    int puntajeBorrarlo = (acPrevios + 1) * (acPrevios + 1) + puntajeMaximo(inicio + 1, fin, 0);
+    mejorPuntaje = puntajeBorrarlo;
+    //Opcion de no borrarlo
+
+    for(int indiceMismoColor = inicio + 1; indiceMismoColor <= fin; indiceMismoColor++){
+        //Si encuentro otro fragmento del mismo color:
+        //  a) Primero eliminamos lo que hay en el medio [inicio+1...indiceMismoColor-1]
+        //  b) Luego unimos inicio con indiceMismoColor (ahora con acPrevios+1)
+        if(fragmentos[indiceMismoColor] == fragmentos[inicio]){
+           int puntajeNoBorrarlo = puntajeMaximo(inicio+1, indiceMismoColor-1, 0) + puntajeMaximo(indiceMismoColor, fin, acPrevios + 1);
+           mejorPuntaje = max(mejorPuntaje, puntajeNoBorrarlo);
+        }
+    }
+    return pd[inicio][fin][acPrevios] = mejorPuntaje;
+}
+
 int main()
 {
-    // TODO
+    int cantFragmentos;
+    cin >> cantFragmentos;
+    int * fragmentos = new int[cantFragmentos];
+    for(int i = 0; i < cantFragmentos; i++){
+        cin >> fragmentos[i];
+    }
+    cout << puntajeMaximo(0, cantFragmentos, 0, pd);
     return 0;
 }
